857ec1e7758f2c1aa935a41bcda2cfd86c403111
Yago Gomes <yagogomesc@gmail.com>
2022-08-08 22:13:52 -0300

    First commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    try {
+
+      const {user_id} = request.headers;
+      const users = this.listAllUsersUseCase.execute({user_id} as 
+        {user_id: string});
+  
+      return response.json(users)
+    }catch(err) {
+      return response.status(400).json({error: err})
+    }
+  }
+}
+
+export { ListAllUsersController };
---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,24 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const userIsAdmin = this.usersRepository.findById(user_id);
+
+    if (!userIsAdmin.admin) {
+      throw new Error('You area not an admin');
+    }
+
+    const users = this.usersRepository.list();
+
+    return users
+  }
+}
+
+export { ListAllUsersUseCase };
